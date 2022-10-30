8e4a6f644e8da09196d58026752cbf2caf2efe34
Rafaela Carneiro <94813140+rafaelamscarneiro@users.noreply.github.com>
2022-10-30 18:36:27 -0300

    Update Jogo.java

--- src/Model/Jogo.java
+++ src/Model/Jogo.java
@@ -49,9 +49,16 @@ class Jogo {
 	
 	
 	
-	public int acusar(Carta c[]) {
-		 //talez mudar td para treeSet
-		return -1;
+	public boolean acusar(Carta acusacao[]) {
+		 //talvez mudar td para treeSet
+		for(int i=0; i<3; i++) {
+			for(int j=0; j<3; j++) {
+				if(!acusacao[j].equals(cartasAssassino[i])) {
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 	
 	public String darPalpite(Carta palpites[]) {

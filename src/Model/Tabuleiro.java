3de9303d01513ba18bd689f694912bc6356a01e8
Eduardo Luna <edudaluna2000@gmail.com>
2022-11-04 12:50:00 -0300

    Deleted Find the Complete path

--- src/Model/Tabuleiro.java
+++ src/Model/Tabuleiro.java
@@ -35,42 +35,6 @@ class Tabuleiro {
 		return posicoes[x][y];
 	}
 	
-
-	  ArrayList<Coordenadas> achaPosFinais(int x, int y, int casas, int turno){
-		
-		ArrayList<Coordenadas> aux = new ArrayList<Coordenadas>();
-
-		aux.addAll(__recAchaPosFinais(x,y,casas+1,turno));
-		//printAsMap(aux);
-		
-		return aux;
-	}
-	
-	private ArrayList<Coordenadas> __recAchaPosFinais(int x, int y, int casas, int turno){
-		ArrayList<Coordenadas> res = new ArrayList<Coordenadas>();
-		ArrayList<Coordenadas> prox = new ArrayList<Coordenadas>();
-		Posicao posAtual;
-		Coordenadas coordAtual;
-		posAtual = getPosicaoAt(x,y);
-		coordAtual = posAtual.getCoordenadas();
-		if(casas>0) {
-			res = new ArrayList<Coordenadas>();
-			res.add(coordAtual);
-			coordAtual.setPassouAqui(true);
-			for(Coordenadas c: posAtual.getPosicoesProximas()) {
-				if(c!=null) {
-					if(!c.getPassouAqui()) {
-						prox = __recAchaPosFinais(c.getX(),c.getY(),casas-1,turno);
-						if(prox!=null) {
-							res.addAll(prox);
-						}
-					}
-				}
-			}
-			coordAtual.setPassouAqui(false);
-		}
-		return res;
-	}
 	
 	void houseKeepingTabuleiro() {
 		for(Posicao arrayPos[] : posicoes) {

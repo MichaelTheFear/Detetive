31260bdb691d1d4604117e1fa8f7def273257a2a
Eduardo Fontoura <91386962+Raygrou@users.noreply.github.com>
2022-11-05 14:08:11 -0300

    Format and changes Middleware methods

--- src/Controller/Middleware.java
+++ src/Controller/Middleware.java
@@ -1,55 +1,50 @@
 package Controller;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
-import Model.Jogo;
+import Model.JogoFacade;
 import Util.*;
 import View.Canvas;
 
 public class Middleware {
-	Jogo model = new Jogo();
-	
-	
-	Middleware(){
-		//Canvas canvas = new Canvas();
-		//JogoFacade model = new JogoFacade();
+	static JogoFacade model = JogoFacade.getJogoFacade();
+
+	Middleware() {
+		// Canvas canvas = new Canvas();
+		// JogoFacade model = new JogoFacade();
 		initBoard();
 		initRolarDados();
 	}
-	
-	
+
 	private void initBoard() {
-		
+
 		Canvas.onGameStart(new FunctionClass() {
 
 			@Override
 			public void callbackVoid() {
-				
-				
-				
+
 			}
-			
+
 		});
 	}
-	
-	
+
 	private void initRolarDados() {
 		Canvas.onRolarDados(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				model.rolarDado();
+				model.rolarDados();
 				Canvas.setDados(model.getDados());
 			}
-			
+
 		});
 	}
-	
+
 	public static void main(String[] agrs) {
 		new Middleware();
-		
+
 	}
-	
-	
+
 }
